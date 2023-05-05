fa0d87640bdcc4baab0fc4d7ff9107c9762f3efa
saeed <saeedblack77@gmail.com>
2023-05-05 19:45:14 +0330

    khoone 1402/02/15 17:57 first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    app: {
+        head: {
+            charset: "utf-16",
+            viewport: "width=device-width,initial-scale=1",
+            title: "Elon's Blog",
+        }
+    },
+// https://toolsnull.com/code-solution/the-best-way-to-include-bootstrap-in-nuxtjs
+    css: [
+        '~/node_modules/bootstrap/dist/css/bootstrap.min.css',
+    ],
+    plugins: [
+        // This line has conflict with devtools. resolve it later. saeed.doc
+        // {src: "~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js", mode: "client"}
+    ],
 })
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+  <TheHeader></TheHeader>
+  <div class="m-5">
+    <div class="btn btn-success">Success</div>
+  </div>
+  <h1>
+    elon musk !!!
+  </h1>
+</template>
